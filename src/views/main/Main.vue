2b5a1dc557a334c233959bd2a12460d986e24240
Rae <97735510+RaeZhong@users.noreply.github.com>
2024-02-22 02:45:43 +0800

    feat:主页面框架

--- src/views/main/Main.vue
+++ src/views/main/Main.vue
@@ -1,5 +1,245 @@
 <template>
   <div>
-    
+    <div class="top">
+      <div class="top-op">
+        <div class="btn">
+          <el-upload
+            :show-file-list="false"
+            :with-credentials="true"
+            :multiple="true"
+            :http-request="addFile"
+            :accept="fileAccept"
+          >
+            <el-button type="primary">
+              <span class="iconfont icon-upload"></span>
+              上传
+            </el-button>
+          </el-upload>
+        </div>
+        <el-button type="success" @click="newFolder" v-if="category == 'all'">
+          <span class="iconfont icon-folder-add"></span>
+          新建文件夹
+        </el-button>
+        <el-button
+          @click="delFileBatch"
+          type="danger"
+          :disabled="selectFileIdList.length == 0"
+        >
+          <span class="iconfont icon-del"></span>
+          批量删除
+        </el-button>
+        <el-button
+          @click="moveFolderBatch"
+          type="warning"
+          :disabled="selectFileIdList.length == 0"
+        >
+          <span class="iconfont icon-move"></span>
+          批量移动
+        </el-button>
+        <div class="search-panel">
+          <el-input
+            clearable
+            placeholder="输入文件名搜索"
+            v-model="fileNameFuzzy"
+            @keyup.enter="search"
+          >
+            <template #suffix>
+              <i class="iconfont icon-search" @click="search"></i>
+            </template>
+          </el-input>
+        </div>
+        <div class="iconfont icon-refresh" @click="loadDataList"></div>
+      </div>
+      <!--导航-->
+      <Navigation ref="navigationRef" @navChange="navChange"></Navigation>
+    </div>
+    <div class="file-list" v-if="tableData.list && tableData.list.length > 0">
+      <Table
+        ref="dataTableRef"
+        :columns="columns"
+        :showPagination="true"
+        :dataSource="tableData"
+        :fetch="loadDataList"
+        :initFetch="false"
+        :options="tableOptions"
+        @rowSelected="rowSelected"
+      >
+        <template #fileName="{ index, row }">
+          <div
+            class="file-item"
+            @mouseenter="showOp(row)"
+            @mouseleave="cancelShowOp(row)"
+          >
+            <template
+              v-if="(row.fileType == 3 || row.fileType == 1) && row.status == 2"
+            >
+              <icon :cover="row.fileCover" :width="32"></icon>
+            </template>
+            <template v-else>
+              <icon v-if="row.folderType == 0" :fileType="row.fileType"></icon>
+              <icon v-if="row.folderType == 1" :fileType="0"></icon>
+            </template>
+            <span class="file-name" v-if="!row.showEdit" :title="row.fileName">
+              <span @click="preview(row)">{{ row.fileName }}</span>
+              <span v-if="row.status == 0" class="transfer-status">转码中</span>
+              <span v-if="row.status == 1" class="transfer-status transfer-fail"
+                >转码失败</span
+              >
+            </span>
+            <div class="edit-panel" v-show="row.showEdit">
+              <el-input
+                v-model.trim="row.fileNameReal"
+                ref="editNameRef"
+                :maxLength="190"
+                @keyup.enter="saveNameEdit(index)"
+              >
+                <template #suffix>{{ row.fileSuffix }}</template>
+              </el-input>
+              <span
+                :class="[
+                  'iconfont icon-right1',
+                  row.fileNameReal ? '' : 'not-allow',
+                ]"
+                @click="saveNameEdit(index)"
+              ></span>
+              <span
+                class="iconfont icon-error"
+                @click="cancelNameEdit(index)"
+              ></span>
+            </div>
+            <span class="op">
+              <template v-if="row.showOp && row.fileId && row.status == 2">
+                <span class="iconfont icon-share1" @click="share(row)"
+                  >分享</span
+                >
+                <span
+                  class="iconfont icon-download"
+                  @click="download(row)"
+                  v-if="row.folderType == 0"
+                  >下载</span
+                >
+                <span class="iconfont icon-del" @click="delFile(row)"
+                  >删除</span
+                >
+                <span
+                  class="iconfont icon-edit"
+                  @click.stop="editFileName(index)"
+                  >重命名</span
+                >
+                <span class="iconfont icon-move" @click="moveFolder(row)"
+                  >移动</span
+                >
+              </template>
+            </span>
+          </div>
+        </template>
+        <template #fileSize="{ index, row }">
+          <span v-if="row.fileSize">
+            {{ proxy.Utils.sizeToStr(row.fileSize) }}</span
+          >
+        </template>
+      </Table>
+    </div>
+    <div class="no-data" v-else>
+      <div class="no-data-inner">
+        <Icon iconName="no_data" :width="120" fit="fill"></Icon>
+        <div class="tips">当前目录为空，上传你的第一个文件吧</div>
+        <div class="op-list">
+          <el-upload
+            :show-file-list="false"
+            :with-credentials="true"
+            :multiple="true"
+            :http-request="addFile"
+            :accept="fileAccept"
+          >
+            <div class="op-item">
+              <Icon iconName="file" :width="60"></Icon>
+              <div>上传文件</div>
+            </div>
+          </el-upload>
+          <div class="op-item" v-if="category == 'all'" @click="newFolder">
+            <Icon iconName="folder" :width="60"></Icon>
+            <div>新建目录</div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!--预览-->
+    <Preview ref="previewRef"> </Preview>
+    <!--移动-->
+    <FolderSelect
+      ref="folderSelectRef"
+      @folderSelect="moveFolderDone"
+    ></FolderSelect>
+    <!--分享-->
+    <FileShare ref="shareRef"></FileShare>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      columns: [
+        {
+          label: "文件名",
+          prop: "fileName",
+          scopedSlots: "fileName",
+        },
+        {
+          label: "修改时间",
+          prop: "lastUpdateTime",
+          width: 200,
+        },
+        {
+          label: "大小",
+          prop: "fileSize",
+          scopedSlots: "fileSize",
+          width: 200,
+        },
+      ],
+      tableData: {},
+      tableOptions: {
+        extHeight: 50,
+        selectType: "checkbox",  
+      },
+      selectFileIdList: [],
+      fileNameFuzzy: null,
+      showLoading: true,
+      category: null,
+      editing: false,
+      currentFolder: { fileId: 0,},
+      currentMoveFile: {},
+    };
+  },
+  methods: {
+    addFile() {},
+    reload() {},
+    search() {},
+    rowSelected() {},
+    loadDataList() {},
+    showOp() {},
+    cancelShowOp() {},
+    newFolder() {},
+    editFileName(index) {},
+    cancelNameEdit(index) {},
+    saveNameEdit(index) {},
+    preview(data) {}, 
+    navChange(data) {},
+    moveFolder(data) {},
+    moveFolderBatch() {},
+    moveFolderDone() {},
+    delFile(row) {},
+    delFileBatch() {},
+    download() {},
+    share(row) {},
+  },
+  computed: {
+    fileAccept() {},
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "@/assets/file.list.scss";
+</style>
--- src/views/main/ShareFile.vue
+++ src/views/main/ShareFile.vue
@@ -1,5 +1,129 @@
 <template>
   <div>
-    
+    <Dialog ref="dialogRef" :title="dialogConfig.title" :buttons="dialogConfig.buttons" width="600px"
+      :showCancel="showCancel" @close="dialogConfig.show = false">
+      <el-form :model="formData" :rules="rules" ref="formDataRef" label-width="100px" @submit.prevent>
+        <el-form-item label="文件"> {{ formData.fileName }} </el-form-item>
+        <template v-if="showType == 0">
+          <el-form-item label="有效期" prop="validType">
+            <el-radio-group v-model="formData.validType">
+              <el-radio :label="0">1天</el-radio>
+              <el-radio :label="1">7天</el-radio>
+              <el-radio :label="2">30天</el-radio>
+              <el-radio :label="3">永久有效</el-radio>
+            </el-radio-group>
+          </el-form-item>
+          <el-form-item label="提取码" prop="codeType">
+            <el-radio-group v-model="formData.codeType">
+              <el-radio :label="0">自定义</el-radio>
+              <el-radio :label="1">系统生成</el-radio>
+            </el-radio-group>
+          </el-form-item>
+          <el-form-item prop="code" v-if="formData.codeType == 0">
+            <el-input clearable placeholder="请输入5位提取码" v-model.trim="formData.code" maxLength="5"
+              :style="{ width: '130px' }"></el-input>
+          </el-form-item>
+        </template>
+        <template v-else>
+          <el-form-item label="分享连接" prop="validType">
+            {{ shareUrl }}{{ resultInfo.shareId }}
+          </el-form-item>
+          <el-form-item label="提取码" prop="validType">
+            {{ resultInfo.code }}
+          </el-form-item>
+          <el-form-item prop="validType">
+            <el-button type="primary" @click="copy">复制链接极提取码</el-button>
+          </el-form-item>
+        </template>
+      </el-form>
+    </Dialog>
   </div>
 </template>
+
+<script>
+import Dialog from "@/components/Dialog.vue"
+import verify from "@/utils/Verfiy.js";
+import request from "@/utils/Request.js";
+import message from "@/utils/Message.js";
+
+export default {
+  name: "ShareFile",
+  components: {
+    Dialog,
+  },
+  data() {
+    return {
+      showType: 0,
+      formData: {},
+      rules: {
+        validType: [{ required: true, message: "请选择有效期" }],
+        codeType: [{ required: true, message: "请选择提取码类型" }],
+        code: [
+          { required: true, message: "请输入提取码" },
+          { validator: verify.shareCode, message: "提取码只能是数字字母" },
+          { min: 5, message: "提取码最少5位" },
+        ],
+      },
+      showCancel: true,
+      dialogConfig: {
+        title: "分享",
+        buttons: [
+          {
+            type: "primary",
+            text: "确定",
+            click: (e) => {
+              share();
+            },
+          },
+        ],
+      },
+      resultInfo: {},
+    }
+  },
+  methods: {
+    share() {
+      async () => {
+        if (Object.keys(this.resultInfo).length > 0) {
+          this.$refs.dialogRef.showDialog();
+          return;
+        }
+        this.$refs.formDataRef.validate(async (valid) => {
+          if (!valid) {
+            return;
+          }
+          let params = {};
+          Object.assign(params, this.formData);
+          let res = await request({
+            url: "",
+            params: params,
+          });
+          if (!res) return;
+          this.showType = 1;
+          this.resultInfo = res.data;
+          this.dialogConfig.buttons[0].text = "关闭";
+          this.showCancel = false;
+        })
+      }
+    },
+    show(data) {
+      this.showCancel = true;
+      this.$refs.dialogRef.showDialog();
+      this.showType = 0;
+      this.resultInfo = {};
+      this.$nextTick(() => {
+        this.$refs.formDataRef.resetField();
+        this.formData = Object.assign({}, data);
+      })
+    },
+    copy(){
+      async() => {
+        await this.$copyText(
+          `链接:${shareUrl.value}${resultInfo.value.shareId} 提取码: ${resultInfo.value.code}`
+        ).then(() => {
+          message.success("复制成功");
+        })
+      }
+    }
+  },
+}
+</script>
